149c61d4b088e6bc7e01c91c6aa99f662f89909f
Maddie Lord <maddielord@maddies-air.lan>
2020-12-05 21:37:00 -0800

    clean up

--- day6.js
+++ day6.js
@@ -16,15 +16,11 @@ import _ from 'lodash';
 const input = getInput(6);
 
 function part1() {
-    return input.reduce((a, c) => {
-        return a + _.uniq([...c.split('\n').join('')]).length;
-    }, 0);
+    return input.reduce((a, c) => a + _.uniq([...c.split('\n').join('')]).length, 0);
 }
 
 function part2() {
-    return input.reduce((a, c) => {
-        return a + _.intersection(...c.split('\n').map((x) => x.split(''))).length
-    }, 0);
+    return input.reduce((a, c) => a + _.intersection(...c.split('\n').map((x) => x.split(''))).length, 0);
 }
 
 formatAnswer(part1(), part2());
